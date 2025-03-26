507d08f0bee3bf9152f37814abe956afe5ecbf9c
Arthur VIENOT ONGARO <arthurvienot1008@gmail.com>
2025-03-26 08:45:37 +0100

    settup react router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import { Routes, Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Layout from "./layout/Layout";
-//import page
+import Home from "./pages/Home";
+import Settings from "./pages/Settings";
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
+        <Route index element={<Home/>} />
         <Route path="settings" element={<Settings />} />
       </Route>
     </Routes>
